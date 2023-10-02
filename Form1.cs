64885bd8b511063d2bb479e9dabe45d367917196
Julian Castro <141683762+Juliox14@users.noreply.github.com>
2023-10-02 13:59:51 -0600

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3OLIDST_JulianCastro_02
+{
+    public partial class PrimeraVentana : System.Windows.Forms.Form
+    {
+        public PrimeraVentana()
+        {
+            InitializeComponent();
+        }
+    }
+}
